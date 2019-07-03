58f961d0a3687155f6064a6f1e136ed39057c8af
rhine3 <tessa.rhinehart@gmail.com>
2019-07-03 14:34:08 -0400

    Improve linebreaks in audacity_noise_reduce docstring

--- squiggle_detector_object.py
+++ squiggle_detector_object.py
@@ -806,15 +806,24 @@ frequency and sample boxes computed with Audio.box()')
             nr_params (dict): parameters for Audacity noise reduce.
                 From noisereduce documentation (https://github.com/timsainb/noisereduce)
                 the possible parameters are:
-                    n_grad_freq (int): how many frequency channels to smooth over with the mask.
-                    n_grad_time (int): how many time channels to smooth over with the mask.
+                    n_grad_freq (int): how many frequency channels to smooth 
+                        over with the mask.
+                    n_grad_time (int): how many time channels to smooth over 
+                        with the mask.
                     n_fft (int): number audio of frames between STFT columns.
-                    win_length (int): Each frame of audio is windowed by `window()`. The window will be of length `win_length` and then padded with zeros to match `n_fft`..
-                    hop_length (int):number audio of frames between STFT columns.
-                    n_std_thresh (int): how many standard deviations louder than the mean dB of the noise (at each frequency level) to be considered signal
-                    prop_decrease (float): To what extent should you decrease noise (1 = all, 0 = none)
-                    pad_clipping (bool): Pad the signals with zeros to ensure that the reconstructed data is equal length to the data
-                            use_tensorflow (bool): Use tensorflow as a backend for convolution and fft to speed up computation
+                    win_length (int): Each frame of audio is windowed by 
+                        `window()`. The window will be of length `win_length` 
+                        and then padded with zeros to match `n_fft`..
+                    hop_length (int): number audio of frames between STFT columns.
+                    n_std_thresh (int): how many standard deviations louder 
+                        than the mean dB of the noise (at each frequency level) 
+                        to be considered signal
+                    prop_decrease (float): To what extent should you decrease 
+                        noise (1 = all, 0 = none)
+                    pad_clipping (bool): Pad the signals with zeros to 
+                        ensure that the reconstructed data is equal length to the data
+                    use_tensorflow (bool): Use tensorflow as a backend for
+                        convolution and fft to speed up computation
                     verbose (bool): Whether to plot the steps of the algorithm
         '''
 
